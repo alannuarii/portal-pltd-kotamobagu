d3496336395a5fb441ea3d24b8f0704637e6558c
alannuarii <alannuariii@gmail.com>
2022-02-17 17:32:54 +0800

    Update company profile

--- public/js/script7.js
+++ public/js/script7.js
@@ -13,3 +13,21 @@ new Swiper(".clients-slider", {
     },
   },
 });
+
+// * Initiate glightbox
+const glightbox = GLightbox({
+  selector: ".glightbox",
+});
+
+// Initiate AOS
+function aos_init() {
+  AOS.init({
+    duration: 1000,
+    easing: "ease-in-out",
+    once: true,
+    mirror: false,
+  });
+}
+window.addEventListener("load", () => {
+  aos_init();
+});
